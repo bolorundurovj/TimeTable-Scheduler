3e2e9ead58387cfb47e91f762602ba0dba3d1595
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2022-01-26 01:56:14 +0100

    Initial commit

---
+++ src/com/data_structure/DBConnect.java
@@ -0,0 +1,18 @@
+package com.data_structure;
+import javax.swing.JOptionPane;
+import java.sql.*;
+public class DBConnect {
+    public static Connection con;
+    public static ResultSet rs;
+    public static Statement st;
+    public static void connect(){
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/time_table_scheduling?serverTimezone=UTC", "root", "rootroot");
+            st=con.createStatement();
+        }catch (SQLException e){ 
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Cannot connect to the database","Issue!", JOptionPane.OK_OPTION);
+        } 
+    }
+}
